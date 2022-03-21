e110e614df76344a59b2bd1d8691736129edf863
SR <sjoerd@re-boyds-145-109-147-44.wireless.hva.nl>
2022-03-21 16:08:41 +0100

    ttest

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const name = "Michael";
+const name = 'Michael';
 console.log(`Hello ${name}`);
 
 const camelCase = require('camelcase');
 
 camelCase('foo-bar');
 console.log(camelCase('harry-potter'));
+
+// remove this line
