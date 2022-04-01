e732fca03ae76cc3ad596eeba2a5b7d61c9f59ad
Michael Wilhelm <mwilhelm08@gmail.com>
2022-04-01 13:11:44 +0200

    Bundle en minify

--- public/scripts/currencyApi.js
+++ public/scripts/currencyApi.js
@@ -1,19 +1,26 @@
-const nummer = document.getElementById("nummer");
-let currencyData;
+const currencyContainer = document.getElementById("bereken_section");
+currencyContainer && calculateCurrency()
 
-fetch("http://api.exchangeratesapi.io/v1/latest?access_key=d3aff1dc0f722c3f6697fab137474ed1")
-    .then((data) => data.json())
-    .then((data) => {
-        console.log(data);
-        currencyData = data;
+function calculateCurrency() {
+
+    const nummer = document.getElementById("nummer");
+    let currencyData;
+
+    fetch("http://api.exchangeratesapi.io/v1/latest?access_key=d3aff1dc0f722c3f6697fab137474ed1")
+        .then((data) => data.json())
+        .then((data) => {
+            console.log(data);
+            currencyData = data;
+        });
+
+    nummer.addEventListener("input", () => {
+        const countryCode = document.getElementById('currencyvalue').innerText;
+        const output = document.getElementById('antwoord');
+
+        console.log(nummer.value);
+        console.log(currencyData.rates[countryCode]);
+        const calculatedValue = nummer.value * currencyData.rates[countryCode]
+        output.value = calculatedValue
     });
 
-nummer.addEventListener("input", () => {
-    const countryCode = document.getElementById('currencyvalue').innerText;
-    const output = document.getElementById('antwoord');
-
-    console.log(nummer.value);
-    console.log(currencyData.rates[countryCode]);
-    const calculatedValue = nummer.value * currencyData.rates[countryCode]
-    output.value = calculatedValue
-});
+}
--- public/scripts/weatherAPI.js
+++ public/scripts/weatherAPI.js
@@ -1,28 +1,35 @@
-const fetchWeather = async () => {
+const weatherContainer = document.getElementById("bereken_section");
+weatherContainer && getWeather()
 
-  const countryName = document.getElementById('country-name').innerText
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${countryName}?unitGroup=metric&key=T34UCHC8WQQH83SAKKUC7CLRC&contentType=json`;
+function getWeather() {
 
-  const response = await fetch(url);
-  const data = await response.json();
+  const fetchWeather = async () => {
 
-  setWeatherData(data)
+    const countryName = document.getElementById('country-name').innerText
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${countryName}?unitGroup=metric&key=T34UCHC8WQQH83SAKKUC7CLRC&contentType=json`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setWeatherData(data)
+  }
+
+  const setWeatherData = (data) => {
+    const container = document.getElementById('weather-data')
+
+    const img = document.createElement("img");
+    img.src = `/images/weather-icons/${data.currentConditions.icon}.png`;
+    container.appendChild(img);
+
+    const temp = document.createElement("p");
+    temp.innerText = `Tempratuur: ${data.currentConditions.temp}`;
+    container.appendChild(temp);
+
+    const wind = document.createElement("p");
+    wind.innerText = `Windsnelheid: ${data.currentConditions.windspeed} km/uur`;
+    container.appendChild(wind);
+  }
+
+  fetchWeather()
+
 }
-
-const setWeatherData = (data) => {
-  const container = document.getElementById('weather-data')
-
-  const img = document.createElement("img");
-  img.src = `/images/weather-icons/${data.currentConditions.icon}.png`;
-  container.appendChild(img);
-
-  const temp = document.createElement("p");
-  temp.innerText = `Tempratuur: ${data.currentConditions.temp}`;
-  container.appendChild(temp);
-
-  const wind = document.createElement("p");
-  wind.innerText = `Windsnelheid: ${data.currentConditions.windspeed} km/uur`;
-  container.appendChild(wind);
-}
-
-fetchWeather()
