da0172aee218fc93bd41e8464f3225bc6f43027d
K3A101 <keisha.alexander@hva.nl>
2022-04-03 11:32:06 +0200

    Express validator methode  weghalen

--- middleware/login-validation.js
+++ middleware/login-validation.js
@@ -3,35 +3,24 @@
      validationResult
  } = require('express-validator');
 
- 
+
  exports.validateUserLogin = [
-       check('email').normalizeEmail().isEmail().withMessage('Email is niet geldig'),
+     check('email').normalizeEmail().isEmail().withMessage('Email is niet geldig'),
 
      check('wachtwoord').not().isEmpty().withMessage('Wachtwoord is verplicht!'),
-     
-     check('wachtwoord').exists().custom((value, {
-                 req, res, next
-             }) => {
-                 if (value !== req.body.wachtwoord) {
-                     throw new Error('Wachtwoord is niet correct');
-                 }else {
-                     return true
-                 }
-                }
-                 
-                 )
-                ]
 
-exports.loginResult = (req, res, next) => {
-    const result = validationResult(req).array();
-    const loggedInUser = req.session.user ? req.session.user : null;
+ ]
 
-    if (result.length > 0) {
-        res.render('aanmelden', {
-            err: result,
-            user: loggedInUser,
-        });
-    } else {
-        return
-    }
-}
+ exports.loginResult = (req, res, next) => {
+     const result = validationResult(req).array();
+     const loggedInUser = req.session.user ? req.session.user : null;
+
+     if (result.length > 0) {
+         res.render('aanmelden', {
+             err: result,
+             user: loggedInUser,
+         });
+     } else {
+         return true
+     }
+ }
